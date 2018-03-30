5b3edbc725e474b85a8d6f635cd0eb2a8ed6d6b4
Jing <jing@aviatrix.com>
2018-03-30 14:29:55 -0700

    update quickstart templates for 3.1 version

--- functions/source/compile-test.py
+++
@@ -1,4 +0,0 @@
-import py_compile
-py_compile.compile('../quickstart-aviatrix-nextgentransithub/functions/source/aviatrix_poller.py')
-py_compile.compile('../quickstart-aviatrix-nextgentransithub/functions/source/aviatrix_controller.py')
-py_compile.compile('../quickstart-aviatrix-nextgentransithub/functions/source/aviatrix_gateway.py')
